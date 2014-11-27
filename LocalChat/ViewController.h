d0e97de596559b35a686db58c80fbb52408ee9e0
Ken Rubesh <kenrubesh@Ken-Rubeshs-MacBook-Pro.local>
2014-11-26 19:31:50 -0800

    class 5 video 1

--- LocalChat/ViewController.h
+++ LocalChat/ViewController.h
@@ -9,7 +9,10 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIView *textEntryView;
 
+@property (weak, nonatomic) IBOutlet UITextField *textInput;
+@property (weak, nonatomic) IBOutlet UIButton *sendText;
 
 @end
 
